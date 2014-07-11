571e1840d49bcc56239bc2cdf55228aa710eb065
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-07-12 00:30:18 +0900

    Convert classes to modules. #5

--- lib/jan/check_digit_calculator.rb
+++ lib/jan/check_digit_calculator.rb
@@ -2,11 +2,11 @@ require "jan/parser"
 
 class Jan
   module CheckDigitCalculator
-    class << self
-      def calculate(code)
-        code = code.to_s + "x"
-        10 - (Parser.even_digits(code).reduce(&:+) * 3 + Parser.odd_digits(code).reduce(&:+)) % 10
-      end
+    module_function
+
+    def calculate(code)
+      code = code.to_s + "x"
+      10 - (Parser.even_digits(code).reduce(&:+) * 3 + Parser.odd_digits(code).reduce(&:+)) % 10
     end
   end
 end
--- lib/jan/validator.rb
+++ lib/jan/validator.rb
@@ -1,13 +1,13 @@
 require "jan/parser"
 
 class Jan
-  class Validator
-    class << self
-      def validate(code)
-        code = code.to_s
-        return false unless [8,13].include?(code.size)
-        Parser.checkdigit(code) == CheckDigitCalculator.calculate(Parser.body(code))
-      end
+  module Validator
+    module_function
+
+    def validate(code)
+      code = code.to_s
+      return false unless [8,13].include?(code.size)
+      Parser.checkdigit(code) == CheckDigitCalculator.calculate(Parser.body(code))
     end
   end
 end
