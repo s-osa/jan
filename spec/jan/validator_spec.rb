fae6a8c32a82e7eff9c6f75d3ad47df1e4b6cdb5
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-07-11 23:03:13 +0900

    Refactor specs. #1

--- spec/jan/validator_spec.rb
+++ spec/jan/validator_spec.rb
@@ -3,13 +3,6 @@ require "spec_helper"
 describe Jan::Validator do
   describe ".validate" do
     context "valid codes" do
-      valid_codes = [
-        "4901277241126",
-        4901277241126,
-        "49123456",
-        49123456
-      ]
-
       valid_codes.each do |code|
         it "#{code} should be valid" do
           actual = Jan::Validator.validate(code)
@@ -19,16 +12,8 @@ describe Jan::Validator do
     end
 
     context "invalid codes" do
-      valid_codes = [
-        "0000000000000",
-        "11",
-        "123abc",
-        "4901277 241126",
-        "4901277 24112"
-      ]
-
-      valid_codes.each do |code|
-        it "#{code} should be valid" do
+      invalid_codes.each do |code|
+        it "#{code} should be invalid" do
           actual = Jan::Validator.validate(code)
           expect(actual).to be false
         end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 project_root = File.join(File.dirname(__FILE__), '..')
 $: << project_root
 
+Dir[File.join(File.dirname(__FILE__), "support", "*")].each {|f| require f }
+
 require 'lib/jan'
