ee3ad201db20f6406f298d12ba4a07cbb2b593c4
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-07-11 23:09:30 +0900

    Jan#valid? #1

--- lib/jan.rb
+++ lib/jan.rb
@@ -2,4 +2,13 @@ require "jan/version"
 require "jan/validator"
 
 class Jan
+  attr_accessor :code
+
+  def initialize(code)
+    @code = code
+  end
+
+  def valid?
+    Validator.validate(@code)
+  end
 end
