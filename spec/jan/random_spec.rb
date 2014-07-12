e440cf4d685390103303e6ea59394aaf6c6a1c76
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-07-12 21:38:01 +0900

    Jan::Random.instore_code(size) #3

--- spec/jan/random_spec.rb
+++ spec/jan/random_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 describe Jan::Random do
   describe ".code(size)" do
     (6..20).each do |size|
-      it "should return valid code" do
-        code = Jan::Random.code
+      it "should return code with valid check digit" do
+        code = Jan::Random.code(size)
         check_digit = Jan::Parser.check_digit(code)
         actual      = Jan::CheckDigitCalculator.calculate(Jan::Parser.body(code))
         expect(actual).to eq(check_digit)
@@ -12,6 +12,28 @@ describe Jan::Random do
 
       it "should return given length valid code" do
         code = Jan::Random.code(size)
+        expect(code.size).to eq(size)
+      end
+    end
+  end
+
+  describe ".instore_code(size)" do
+    (6..20).each do |size|
+      it "should return code with valid check digit" do
+        code = Jan::Random.instore_code(size)
+        check_digit = Jan::Parser.check_digit(code)
+        actual      = Jan::CheckDigitCalculator.calculate(Jan::Parser.body(code))
+        expect(actual).to eq(check_digit)
+      end
+
+      it "should return instore code" do
+        code   = Jan::Random.instore_code(size)
+        actual = Jan::Parser.instore_code?(code)
+        expect(actual).to be true
+      end
+
+      it "should return given length valid code" do
+        code = Jan::Random.instore_code(size)
         expect(code.size).to eq(size)
       end
     end
