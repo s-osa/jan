917b3cdc7c9cec2f235ed619a006d2a801ac4935
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-07-12 20:18:39 +0900

    Jan::CheckDigitCalculator returns correct answer in 0 check digit case. #4

--- lib/jan/check_digit_calculator.rb
+++ lib/jan/check_digit_calculator.rb
@@ -4,9 +4,10 @@ class Jan
   module CheckDigitCalculator
     module_function
 
-    def calculate(code)
-      code = code.to_s + "x"
-      10 - (Parser.even_digits(code).reduce(&:+) * 3 + Parser.odd_digits(code).reduce(&:+)) % 10
+    def calculate(body)
+      code = body.to_s + "x"
+      digit = 10 - (Parser.even_digits(code).reduce(&:+) * 3 + Parser.odd_digits(code).reduce(&:+)) % 10
+      digit % 10
     end
   end
 end
--- spec/jan/check_digit_calculator_spec.rb
+++ spec/jan/check_digit_calculator_spec.rb
@@ -6,5 +6,10 @@ describe Jan::CheckDigitCalculator do
       actual = Jan::CheckDigitCalculator.calculate("490127724112")
       expect(actual).to eq(6)
     end
+
+    it "should return 0 as check digit" do
+      actual = Jan::CheckDigitCalculator.calculate("490330111020")
+      expect(actual).to eq(0)
+    end
   end
 end
