d27c7ae8c49c9c09bbbbf63145dcaa3c7e047ca3
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-07-13 22:36:26 +0900

    Jan::Validator.validate_size(code)

--- lib/jan/validator.rb
+++ lib/jan/validator.rb
@@ -7,8 +7,12 @@ class Jan
 
     def validate(code)
       code = code.to_s
-      return false unless [8,13].include?(code.size)
+      return false unless validate_size(code)
       Parser.check_digit(code) == CheckDigitCalculator.calculate(Parser.body(code))
+    end
+
+    def validate_size(code)
+      [8,13].include?(code.to_s.size)
     end
   end
 end
--- spec/jan/validator_spec.rb
+++ spec/jan/validator_spec.rb
@@ -20,4 +20,28 @@ describe Jan::Validator do
       end
     end
   end
+
+  describe ".validate_size" do
+    context "valid codes" do
+      valid_size_codes = [8, 13].map{|n| "x" * n }
+
+      valid_size_codes.each do |code|
+        it "#{code} should be valid" do
+          actual = Jan::Validator.validate_size(code)
+          expect(actual).to be true
+        end
+      end
+    end
+
+    context "invalid codes" do
+      invalid_size_codes = [1,2,3,4,5,6,7,9,10,11,12,14,15].map{|n| "x" * n }
+
+      invalid_size_codes.each do |code|
+        it "#{code} should be invalid" do
+          actual = Jan::Validator.validate_size(code)
+          expect(actual).to be false
+        end
+      end
+    end
+  end
 end
